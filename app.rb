61ea5d526d49e76fc43532ee9ec20f9bcc88c2ad
Andrew Pham <69804999+andrewp8@users.noreply.github.com>
2023-11-22 19:37:24 +0000

    finished square

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-
+  erb(:square_new)
 end
+
+get("/square/new"){
+  erb(:square_new)
+}
+get("/square/results"){
+  erb(:square_results)
+}
+
+get("/square_root/new"){
+  erb(:square_root_new)
+}
+get("/square_root/results"){
+  erb(:square_root_results)
+}
