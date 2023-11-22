8e0d0385431bb1eecf805ae1f66a3311b41cc3b0
Andrew Pham <69804999+andrewp8@users.noreply.github.com>
2023-11-22 20:29:28 +0000

    finished payment

--- app.rb
+++ app.rb
@@ -27,5 +27,19 @@ get("/payment/new"){
   erb(:payment_new)
 }
 get("/payment/results"){
+  apr = params.fetch("user_apr").to_f
+  @converted_apr = apr.to_fs(:percentage,{:precision => 4})
+
+  years = params.fetch("user_years").to_f
+  @converted_years = years.to_i
+
+  pv = params.fetch("user_pv").to_f
+  @converted_pv = pv.to_fs(:currency)
+
+  r = (apr/12)/100
+  n= years*12
+
+  result = (r*pv)/(1-(1+r)**-n)
+  @converted_result = result.to_fs(:currency)
   erb(:payment_results)
 }
