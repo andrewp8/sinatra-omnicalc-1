faea82de2dab395fe2855b4e88598d18058b005c
Andrew Pham <69804999+andrewp8@users.noreply.github.com>
2023-11-22 19:55:50 +0000

    updated GET for sqaure and sqaure root in app.rb

--- app.rb
+++ app.rb
@@ -9,6 +9,8 @@ get("/square/new"){
   erb(:square_new)
 }
 get("/square/results"){
+  @number = params.fetch("number")
+  @result = @number.to_f**2
   erb(:square_results)
 }
 
@@ -16,12 +18,14 @@ get("/square_root/new"){
   erb(:square_root_new)
 }
 get("/square_root/results"){
+  @number = params.fetch("number")
+  @result = Math.sqrt(@number.to_f)
   erb(:square_root_results)
 }
 
-get("/square_root/results"){
-  erb(:square_root_results)
+get("/payment/new"){
+  erb(:payment_new)
 }
-get("/square_root/results"){
-  erb(:square_root_results)
+get("/payment/results"){
+  erb(:payment_results)
 }
